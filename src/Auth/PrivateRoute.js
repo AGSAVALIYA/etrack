6f156e655305451c3acbd032069b1ea7bdcebd28
DSigmaDevTeam <50403153+AGSAVALIYA@users.noreply.github.com>
2022-07-15 08:20:36 +0530

    login bug fixed

--- src/Auth/PrivateRoute.js
+++ src/Auth/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 import app from "../firebase";
 import { getDatabase, ref, set, update, get } from "firebase/database";
@@ -7,15 +7,18 @@ import { getDatabase, ref, set, update, get } from "firebase/database";
 
 const PrivateRoute = ({ children }) => {
   const user = JSON.parse(localStorage.getItem("user"));
- 
+  const navigate = useNavigate();
   if(user){
     const email = user.user.email;
     if(email === "akshitgs0504@gmail.com"){
       return children;
     }
+    else{
+      navigate("/login");
+    }
   }
   else{
-    return <Navigate to="/login" />;
+    navigate("/login");
   }
 }
 
